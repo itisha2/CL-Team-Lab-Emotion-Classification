4eba5ab70a27541097ee544e241a17e8c161c478
sarah-sharifian <104323857+sarah-sharifian@users.noreply.github.com>
2022-05-09 11:02:26 +0200

    Add files via upload

--- NaiveBayes.py
+++ NaiveBayes.py
@@ -1,5 +1,6 @@
 import numpy as np
 
+
 class NaiveBayes:
     def __init__(self, df_train, df_test, no_labels):
         self.no_labels = no_labels
@@ -12,16 +13,19 @@ class NaiveBayes:
         self.class_prob = [0] * self.no_labels
         self.evidence_prob = [[0] * len(self.featureSet) for i in range(self.no_labels)]
 
-        self.pred_prob = [[0] * len(self.no_labels) for i in range(df_test.shape[0])]
-
     def naive_bayes_fit(self):
 
         # split data by class
+        print("data size: ", self.N, "\n")
         for i in range(1, self.no_labels + 1):
             self.map_data_by_class[i] = self.df_train[self.df_train['Emotion-Target'] == i]
 
             # compute prior probability of each class P(y=i)
-            self.class_prob[i - 1] = self.map_data_by_class.get(i).shap[0] / self.N
+            self.class_prob[i - 1] = self.map_data_by_class.get(i).shape[0] / self.N
+
+            print("size of class ", i, ": ", self.map_data_by_class.get(i).shape[0])
+
+        print("class probabilities:", self.class_prob, "\n")
 
         # compute likelihood of evidence in each class
         for i in range(1, self.no_labels + 1):
@@ -29,16 +33,21 @@ class NaiveBayes:
                 cdf = self.map_data_by_class.get(i)  # class i
                 self.evidence_prob[i - 1][j] = cdf[cdf[feature] == 1].shape[0] / cdf.shape[
                     0]  # prob. that the feature is 1 in class i
+        print("Evidence Probability: ", self.evidence_prob, "\n")
 
     def naive_bayes_predict(self):
-        pred_prob = []
+        self.df_test["pred_prob"] = pd.Series([])
         for i in range(self.no_labels):
-            # multiply test data feature values by the evidence probabilities
-            evidence_prob = [
-                self.df_test[self.featureSet[j]] * self.evidence_prob[i][j] + (1 - self.df_test[self.featureSet[j]]) * (
-                1 - self.evidence_prob[i][j]) for j in range(len(self.featureSet))]
-            # divide probability by probability of the class
-            pred_prob.append(np.prod(evidence_prob) / self.class_prob[i])
+            # multiply test data feature values by the evidence probabilities (NOMINATOR)
+            self.df_test["prob_nom"] = pd.Series([])
+            for j in range(len(self.featureSet)):
+                feature = self.featureSet[j]
+                self.df_test.loc[self.df_test[feature] == 1]["prob_nom"] *= self.evidence_prob[i][j]
+                self.df_test.loc[self.df_test[feature] == 0]["prob_nom"] *= (1 - self.evidence_prob[i][j])
+            print(self.df_test["prob_nom"], "\n")
+        # divide probability by probability of the class
+        self.df_test["pred_prob"].append(self.df_test["prob_nom"] / self.class_prob[i])
 
         # set class with max prob as predicted class
-        self.df_test["Target-Predict"] = np.argmax(pred_prob) + 1
+        self.df_test["Target-Predict"] = np.argmax(self.df_test["pred_prob"]) + 1
+
